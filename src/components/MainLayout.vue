45d17f4843843331b111a2546b93de2656f15ee0
Dmitry Kanaev <dimmkan@mail.ru>
2023-05-16 16:25:15 +0300

    Init

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <w-flex justify-center class="lh5">
+    <w-form class="xs12 pa1 mt12" style="border: 1px solid darkblue; border-radius: 5px; height: 25%;">
+      <w-input class="mt5" label="Заголовок статьи" :validators="[validators.required]"
+        style="font-size: 16px;"></w-input>
+
+      <w-textarea class="mt5" label="Текст статьи" :validators="[validators.required]"
+        style="font-size: 16px;">
+      </w-textarea>
+
+      <w-flex justify-center>
+        <w-button xl class="mt5" type="submit" style="font-size: 24px;">Добавить запись</w-button>
+      </w-flex>
+    </w-form>
+  </w-flex>
+</template>
+
+<script>
+export default {
+
+  data: () => ({
+    validators: {
+      required: value => !!value || 'Поле должно быть заполнено'
+    }
+  })
+}
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(WaveUI, {
+  theme: 'light',
+  css: {
+    grid: 24
+  }
+});
+app.mount('#app');
